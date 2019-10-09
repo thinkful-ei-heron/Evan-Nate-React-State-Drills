b992afd556cfac3a2e77d25b3998a3f9d8419630
Evan Vogts <evanjvogts@gmail.com>
2019-10-09 12:00:44 -0500

    finished 2nd drill

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import Bomb from './state-drills/Bomb';
 import './index.css';
-import HelloWorld from './state-drills/HelloWorld';
 
-ReactDOM.render(<HelloWorld />, document.getElementById('root'));
+ReactDOM.render(<Bomb />, document.getElementById('root'));
---
+++ src/state-drills/Bomb.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+export default class Bomb extends React.Component {
+    state = {
+        count: 0
+    };
+
+    componentDidMount() {
+        this.interval = setInterval(() => {
+        let newInterval = this.state.count + 1;
+        this.setState({ 
+            count: newInterval
+        });
+        }, 1000)
+      }
+
+    bombNoise() {
+        if (this.state.count >= 8) {
+        clearInterval(this.interval);
+        return 'BOOM';
+    }
+        else return this.state.count % 2 === 0 ? 'tick' : 'tock'
+    }
+    render() {
+        return (
+        <div>
+            <p>{this.bombNoise()}</p>
+        </div>
+        )
+    }
+}
